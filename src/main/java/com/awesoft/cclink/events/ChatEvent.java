e55dd6de5436776f095f84827234c35a7231bf69
riley <minecraftbossdj@gmail.com>
2025-09-21 15:34:14 -0400

    fixed an issue where you couldnt chat if you had integrated link in inventory

--- src/main/java/com/awesoft/cclink/events/ChatEvent.java
+++ src/main/java/com/awesoft/cclink/events/ChatEvent.java
@@ -27,9 +27,21 @@ public class ChatEvent {
         Set<Item> items = Set.of(ItemRegistry.LINK_CORE.get(),ItemRegistry.LINK_CORE_COMMAND.get(),ItemRegistry.INTEGRATED_LINK_CORE.get());
         if (player.getInventory().hasAnyOf(items)) {
             for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
-                if (player.getInventory().getItem(i).getItem() == ItemRegistry.LINK_CORE.get().asItem() || player.getInventory().getItem(i).getItem() == ItemRegistry.LINK_CORE_COMMAND.get().asItem() || player.getInventory().getItem(i).getItem() == ItemRegistry.INTEGRATED_LINK_CORE.get().asItem()) {
+                if (player.getInventory().getItem(i).getItem() == ItemRegistry.LINK_CORE.get().asItem() || player.getInventory().getItem(i).getItem() == ItemRegistry.LINK_CORE_COMMAND.get().asItem()) {
                     ItemStack item = player.getInventory().getItem(i);
                     ServerComputer computer = ComputerLib.getServerComputer(event.getPlayer().getServer(), item);
+                    if (computer != null) {
+                        Container itemInv = ComputerLib.getContainerFromItem(item);
+                        if (itemInv != null && ComputerLib.hasUpgrade("cclink:chatty_upgrade",item)) {
+                            Object[] args = new Object[2];
+                            args[0] = player.getGameProfile().getName();
+                            args[1] = message;
+                            computer.queueEvent("chat_message",args);
+                        }
+                    }
+                } else if (player.getInventory().getItem(i).getItem() == ItemRegistry.INTEGRATED_LINK_CORE.get().asItem()) {
+                    ItemStack item = player.getInventory().getItem(i);
+                    ServerComputer computer = ComputerLib.getIntegratedServerComputer(ServerContext.get(event.getPlayer().server).registry(), item);
                     if (computer != null) {
                         Container itemInv = ComputerLib.getContainerFromItem(item);
                         if (itemInv != null && ComputerLib.hasUpgrade("cclink:chatty_upgrade",item)) {
